a75072640857c3097e2c421a3d8fc0db8ba4d587
GeorgePaiva <george.paiva15@gmail.com>
2022-09-14 21:34:56 -0300

    feat: update controller.

--- src/main/java/br/com/kyrius/apilanguages/controllers/ApiLanguagesController.java
+++ src/main/java/br/com/kyrius/apilanguages/controllers/ApiLanguagesController.java
@@ -3,12 +3,12 @@ package br.com.kyrius.apilanguages.controllers;
 import br.com.kyrius.apilanguages.domain.Language;
 import br.com.kyrius.apilanguages.infrastructure.repository.LanguageRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 public class ApiLanguagesController {
@@ -17,14 +17,32 @@ public class ApiLanguagesController {
     private LanguageRepository languageRepository;
 
     @GetMapping(value = "/languages")
-    public List<Language> getLanguages() {
+    public ResponseEntity<List<Language>> findAllLanguages() {
         List<Language> languages = languageRepository.findAll();
-        return languages;
+        return new ResponseEntity<List<Language>>(languages, HttpStatus.OK);
+    }
+
+    @GetMapping(value = "/language/{id}")
+    public ResponseEntity<Language> findByIdLanguage(@PathVariable String id) {
+        Optional<Language> languageOptional = languageRepository.findById(id);
+        return new ResponseEntity<Language>(languageOptional.get(), HttpStatus.OK);
     }
 
     @PostMapping("/language")
-    public Language insertLanguage(@RequestBody Language language) {
-        return languageRepository.save(language);
+    public ResponseEntity<Language> insertLanguage(@RequestBody Language language) {
+        return new ResponseEntity<Language>(languageRepository.save(language), HttpStatus.CREATED);
+    }
+
+    @PatchMapping("/language/{id}/{ranking}")
+    public ResponseEntity<Language> updateLanguage(@PathVariable String id, @PathVariable int ranking) {
+        try {
+            Optional<Language> languageOptional = languageRepository.findById(id);
+            languageOptional.get().setRanking(ranking);
+            return new ResponseEntity<Language>(languageRepository.save(languageOptional.get()), HttpStatus.CREATED);
+        } catch (Exception e) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+
     }
 
 
--- src/main/java/br/com/kyrius/apilanguages/domain/Language.java
+++ src/main/java/br/com/kyrius/apilanguages/domain/Language.java
@@ -5,14 +5,20 @@ import lombok.Getter;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
+import java.io.Serializable;
+
 @Getter
 @AllArgsConstructor
 @Document(collection = "LanguagesMains")
-public class Language {
+public class Language implements Serializable {
 
     @Id
     private final String id;
     private final String title;
     private final String image;
-    private final int ranking;
+    private int ranking;
+
+    public void setRanking(final int ranking) {
+        this.ranking = ranking;
+    }
 }
